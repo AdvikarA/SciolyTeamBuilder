03b967f9aeec917e37447e8cb56426ec628f5616
Advikar <83314187+AdvikarA@users.noreply.github.com>
2023-11-24 22:03:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def remove_from_team():
     if remove_person:
         for team_table in [st.session_state.team_a_table, st.session_state.team_b_table]:
             for spot in ["Spot 1", "Spot 2"]:
-                team_table.replace({remove_person: pd.NA}, inplace=True)
+                team_table.replace({remove_person: ''}, inplace=True)
         st.sidebar.success(f"{remove_person} removed from both teams.")
 
 if __name__ == "__main__":
